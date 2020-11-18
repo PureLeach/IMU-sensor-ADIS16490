c232a4818b8ea97f6ef2dc0a289033cda5483d80
Maxim Barannikov <73995503+barannikovMU@users.noreply.github.com>
2020-11-18 22:59:26 +0300

    Update ADIS16490.py

--- ADIS16490.py
+++ ADIS16490.py
@@ -1,15 +1,11 @@
 import spidev                   #Библиотека для работы с интерфейсом SPI
 import time                     #для работы со временем
-import RPi.GPIO as GPIO
+
 #Инициализация интерфейса SPI
 spi=spidev.SpiDev()             #Создаём объект SPI
 spi.open(0, 0)                  #Выбор номера порта и номера устройства(CS) шины SPI 
 spi.max_speed_hz=15000000       #Задаём максимальную скорость работы шины SPI
 spi.mode=3                      #Выбор режима работы SPI (от 0 до 3)
-GPIO.setmode(GPIO.BOARD)        #Инициализация по номеру пина на гребенке коннектора GPIO
-GPIO.setup(29, GPIO.IN)         #Установка режима работы канала на вывод
-GPIO.setup(37, GPIO.IN)         #Установка режима работы канала на вывод
-
 
 def SpiRead(spi,reg):           #Функция считывания данных по SPI
     send=[0]*2                  #Создаём список из двух элементов
@@ -45,10 +41,6 @@ def acc_out(acc):               #Функция обработки данных 
 #Главный цикл
 try:
     while True:
-        ch_state1 = GPIO.input(29)
-        print('ch_state 29', ch_state1)
-        ch_state2 = GPIO.input(37)
-        print('ch_state 37', ch_state2)
         SpiWrite(spi,0x00,0x00)      #Переключаемся на 0 стр
         print('SYS_E_FLAG (0x0 = нет ошибок)', hex(SpiRead(spi,0x08)))
         print('DIAG_STS (0x0 = нет ошибок)', hex(SpiRead(spi,0x0A)))
@@ -71,10 +63,8 @@ try:
         temp = temp_out(temp)
         
         SpiWrite(spi,0x00,0x03)      #Переключаемся на 3 стр
-        SpiWrite(spi,0x0D,0x00)      #Меняем DEC_RATE    0x10
-        SpiWrite(spi,0x0C,0x00)      #Меняем DEC_RATE    0x99
-
-
+        SpiWrite(spi,0x0D,0x10)      #Меняем DEC_RATE    
+        SpiWrite(spi,0x0C,0x99)      #Меняем DEC_RATE    
         
         #Вывод данных в консоль
         print ("Температура:   ", "{:10.4f}".format(temp), " degС")
@@ -84,13 +74,10 @@ try:
         print ("X акселерометр:   ", "{:10.4f}".format(xacc), " mg")
         print ("Y акселерометр:   ", "{:10.4f}".format(yacc), " mg")
         print ("Z акселерометр:   ", "{:10.4f}".format(zacc), " mg")
-        print('NULL_CNFG (должен быть = 0x70A на 3 стр) ', hex(SpiRead(spi,0x0E)))
-        print('Decrate в 16-ричной системе ', hex(SpiRead(spi,0x0C)))
-        print('Decrate в 10-чной системе  ', SpiRead(spi,0x0C))
+        print('NULL_CNFG (должен быть = 0x70A на 3 стр) ', hex(SpiRead(spi,0x0E))))
+        print('Decrate', SpiRead(spi,0x0C))
         time.sleep(1)               #Задержка между обновлением данных
-
 #Выход из цикла
 except KeyboardInterrupt:           #Нажатие Ctrl+C
     spi.close()                     #Отключаем подключение шины SPI         
-    GPIO.cleanup()
 
